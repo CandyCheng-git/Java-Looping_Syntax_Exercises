6c6ce63646719311f3967f526e4cbd2bb77a5a09
CandyCheng-git <loktung06cheng@gmail.com>
2024-10-27 17:24:24 +1100

    Updated Question 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,16 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        // Question 1:
-        // Answer = 18
+        //TIP Question 1:
         int z = 0;
         for (int x = 0; x < 4; x++){
             for (int y = 0; y < 3; y++){
                 z += x;
             }
         }
-        System.out.println(z); //
+        System.out.println(z);
+            //TIP Answer = 18
     }
 }
